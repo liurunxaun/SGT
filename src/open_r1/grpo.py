b7738d666210a52baf32a5d53b10685f0e18dc4d
cs_holder <hetao.cs.hit@gmail.com>
2025-11-15 15:56:03 +0800

    创建新环境

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -28,6 +28,7 @@ from open_r1.utils.callbacks import get_callbacks
 from open_r1.utils.wandb_logging import init_wandb_training
 from trl import GRPOTrainer, ModelConfig, TrlParser, get_peft_config
 
+sys.path.append("/data/home/the/rxliu/projects/open-r1-main")
 
 logger = logging.getLogger(__name__)
 
